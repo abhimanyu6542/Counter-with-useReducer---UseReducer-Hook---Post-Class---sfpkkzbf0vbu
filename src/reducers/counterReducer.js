846a980c83cb68149ac25b5e8a7de91ea5bfb04a
Abhimanyu Debata <107947041+abhimanyu6542@users.noreply.github.com>
2022-09-02 21:16:12 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,4 +1,16 @@
 const counterReducer = (state,action) =>{
+  
+  const INCREMENT = 'increment';
+     const DECREMENT = 'decrement';
+
+    switch (action.type) {
+        case INCREMENT:
+            return { count: state.count + 1 };
+        case DECREMENT:
+            return { count: state.count - 1 };
+        default:
+            return state;
+    }
 
 }
 
